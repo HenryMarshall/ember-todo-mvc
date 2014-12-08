1e0cc7b21da87f2e532f2f391d1f15b372146fb6
hbaughman <hbaughman741@gmail.com>
2014-12-08 11:45:55 -0500

    Add child routes

--- js/router.js
+++ js/router.js
@@ -1,5 +1,7 @@
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+    // Additional child routes will go here later
+  });
 });
 
 // This lets us iterate over {{#each todo in model}}
@@ -8,3 +10,9 @@ Todos.TodosRoute = Ember.Route.extend({
     return this.store.find('todo');
   }
 });
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
+})
