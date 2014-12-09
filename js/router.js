1c86474275d10a530b9da4cd2df14c029961c2d8
hbaughman <hbaughman741@gmail.com>
2014-12-08 19:34:00 -0500

    Implement filter for inactive and link to all

--- js/router.js
+++ js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function() {
   this.resource('todos', { path: '/' }, function() {
     this.route('active');
+    this.route('completed')
   });
 });
 
@@ -21,6 +22,19 @@ Todos.TodosActiveRoute = Ember.Route.extend({
   model: function() {
     return this.store.filter('todo', function(todo) {
       return !todo.get('isCompleted');
+    });
+  },
+  // The renderTemplate method lets us reuse the index template
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller})
+  }
+});
+
+// This seems very not DRY...
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
     });
   },
   renderTemplate: function(controller) {
